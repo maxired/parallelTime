8d640832b4f51a0481a142f78591cfc643e2ac70
Maxence Dalmais <maxence.dalmais@worldline.com>
2015-01-07 15:05:43 +0100

    add files after Github Init

---
+++ index.js
@@ -0,0 +1,68 @@
+
+var totalTime = 0;
+var measureStarted = 0;
+var measureStopped = 0;
+
+var unitTimeStart = undefined;
+var unitTime = 0;
+
+function Measure(){
+  var self = this;
+
+    measureStarted++;
+  var measureTimeStart = process.hrtime();
+  unitTimeStart = unitTimeStart || measureTimeStart;
+  return { stop : function(){
+    var measureTimeStop = process.hrtime();
+    measureStopped++;
+    if( measureStopped == measureStarted ) {
+      // no more running
+      unitTime   += (measureTimeStop[0] - unitTimeStart[0]) * 1000000000 + ( measureTimeStop[1] - unitTimeStart[1]);
+      unitTimeStart = undefined;
+    }
+
+    totalTime += (measureTimeStop[0] - measureTimeStart[0]) * 1000000000 + ( measureTimeStop[1] - measureTimeStart[1]);
+  }
+
+  }
+};
+
+
+var debugging = false;
+module.exports = {
+
+startMeasure : function(){
+                 return new Measure();
+               },
+stopMeasure : function( measure){
+                measure.stop();
+              },
+getTotalHrTime : function(){
+                   return [ Math.floor(totalTime / 1000000000) , totalTime % 1000000000];
+                 },
+debugTime : function(interval){
+              if(debugging) return;              
+              debugging  = true;
+
+              interval = interval || 2000;
+              setInterval( function( ) { 
+                  console.log('[parallelTime]', 'total time is ', module.exports.getTotalHrTime());
+                  console.log('[parallelTime]', 'unit time is ', [ Math.floor(unitTime/1000000000) , unitTime%1000000000 ]);
+                  console.log('[parallelTime]', 'finished measure is ', measureStopped);
+                  console.log('[parallelTime]', 'running measure is ', measureStarted-measureStopped);
+
+                  measureStopped &&     console.log('[parallelTime]', 'mean time is ',  Math.floor(unitTime/measureStopped/1000), "ns");
+
+                  }, interval );
+            }
+};
+
+
+var debugEnviron = process.env.NODE_DEBUG || '';
+var set = "parallelTime";
+if(new RegExp('\\b' + set + '\\b', 'i').test(debugEnviron)) {
+  module.exports.debugTime(2000);
+}
+
+
+
